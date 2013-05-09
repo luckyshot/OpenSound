5fce9170662abc200dbc74a04bdcb332917fa272
Xavi <xavi@xaviesteve.com>
2013-05-09 23:39:51 +0100

    Simplified to just one param

--- config.php
+++ config.php
@@ -5,11 +5,6 @@ $config = array(
 	// Internal starting path of Music folder
 	'path' => 'C:\Documents and Settings\Xavi\My Documents\My Music\Crystal Stilts\Alight of Night', // no trailing slash
 	
-	// Network URL to website
-	'url' => 'http://192.168.1.36/opensound/',
-
-	'database' => 'opensound', // .json
-
 );
 
 
