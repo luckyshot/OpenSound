8e87e688789ad4d88e47615fc2d240e6506a985a
Xavi <xavi@xaviesteve.com>
2013-05-12 16:43:27 +0100

    msg, clients, browse and more

--- opensound.dev.js
+++ opensound.dev.js
@@ -1,6 +1,9 @@
 OpenSound = {
 	config: {
-		interval: 5
+		interval: 5 // seconds
+	},
+	msg: function(msg, class) {"use strict";
+		console.log(msg);
 	},
 	status: function(devicename) {"use strict";
 		$.ajax({
@@ -25,7 +28,7 @@ OpenSound = {
 			$('#vol').val(response.vol);
 			document.getElementById('audio').volume = response.vol/100;
 			// Song info
-			$('#artist').html(response.song);
+			$('#track').html(response.song);
 			//$('#track').html(response.song);
 		});
 	},
@@ -42,7 +45,12 @@ OpenSound = {
 		$.ajax({
 			url: "clients/"
 		}).done(function( response ) {
-			console.log(response);
+			var clients = $('#clients>tbody');
+			clients.empty();
+			for (var i = 0; i < response.length; i++) {
+				
+				clients.append( '<tr><td><i class="ico"></i> '+response[i].name+'</td><td><input type="range" class="device-vol" data-devicename="'+response[i].name+'" min="0" max="100" step="1" value="'+response[i].vol+'"></td><td><input type="checkbox" class="device-status" data-devicename="'+response[i].name+'"'+((response[i].status===1)?' checked':'')+'></td></tr>' );
+			}
 		});
 	},
 	file: function(file){"use strict";
@@ -56,35 +64,53 @@ OpenSound = {
 		$.ajax({
 			url: "browse/"+path
 		}).done(function( response ) {
-			console.log(response);
+			$('#path').html( response.path );
+			$('#browser').html('<li>..</li>');
+			for(var i in response.files) {
+				$('#browser').append('<li>'+response[i]+'</li>');
+			}
 		});
 	},
 	add: function(file){"use strict";
 		$.ajax({
 			url: "add/"+file
 		}).done(function( response ) {
-			console.log(response);
+			if (response.status===1) {
+				this.status();
+			}else{
+				this.msg(response.msg);
+			}
 		});
 	},
 	remove: function(file){"use strict";
 		$.ajax({
 			url: "remove/"+file
 		}).done(function( response ) {
-			console.log(response);
+			if (response.status===1) {
+				this.status();
+			}else{
+				this.msg(response.msg);
+			}
 		});
 	},
 	play: function(file){"use strict";
 		$.ajax({
 			url: "play/"+file
 		}).done(function( response ) {
-			console.log(response);
+			var audio = document.getElementById('audio');
+			audio.src = 'file/'+file;
+			audio.play();
 		});
 	},
 	pause: function(){"use strict";
 		$.ajax({
-			url: "pause/"+file
+			url: "pause"
 		}).done(function( response ) {
-			console.log(response);
+			if (response.status===1) {
+				this.status();
+			}else{
+				this.msg(response.msg);
+			}
 		});
 	},
 	pos: function(pos){"use strict";
@@ -117,23 +143,29 @@ OpenSound = {
  *	EVENTS
  */
 $('#playlist').on('click', 'li', function() {
-	$.ajax({
-		url: "play/"+encodeURIComponent($(this).html())
-	}).done(function( response ) {
-		var audio = document.getElementById('audio');
-		audio.src = 'file/'+$(this).html();
-		audio.play();
-	});
+	OpenSound.play( encodeURIComponent($(this).html()) );
 });
-$('#vol').on('change', function() {
-	document.getElementById('audio').volume = $(this).val()/100;
+
+
+$('#clients').on('change', 'input.device-vol', function() {
+	if ($(this).data('devicename')===localStorage.devicename) {
+		document.getElementById('audio').volume = $(this).val()/100;
+	}
 	$.ajax({
-		url: "vol/"+$(this).val()
+		url: 'client/'+encodeURIComponent($(this).data('devicename'))+'/vol/'+$(this).val()
 	}).done(function( response ) {
-		console.log('vol (change):');
 		console.log(response);
 	});
 });
+
+$('#clients').on('change', 'input.device-status', function() {
+	$.ajax({
+		url: 'client/'+encodeURIComponent($(this).data('devicename'))+'/'+(($(this).prop('checked'))?'on':'off')
+	}).done(function( response ) {
+		console.log(response);
+	});
+});
+
 
 $('#devicename').on('input paste', function() {
 	OpenSound.name( $(this).val() );
@@ -150,6 +182,7 @@ $(document).ready(function() {
 	
 	// Request server data
 	OpenSound.playlist();
+	OpenSound.clients(); // TODO: remove once we do this in status() hash check
 	OpenSound.status();
 	
 	// Set status timer
