87df444ceed1785850728d05243099ada8a0cc22
Xavi <xavi@xaviesteve.com>
2013-05-12 10:34:29 +0100

    testing on apple

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = array(
 	
 	// Internal starting path of Music folder
-	'path' => 'C:\Documents and Settings\Xavi\My Documents\My Music\Crystal Stilts\Alight of Night', // no trailing slash
+	'path' => '/Users/apple/Music', // no trailing slash
 	
 );
 
