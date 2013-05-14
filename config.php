c27ccd47a28d682c46eb5611a31ba1b212014ee4
Xavi <xavi@xaviesteve.com>
2013-05-14 12:22:00 +0100

    working on macbook so i've updated these to it

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 $config = array(
 	
 	// Internal starting path of Music folder
-	'path' => '/Users/apple/Music', // no trailing slash
-	
+	// iMac: 'path' => '/Users/apple/Music', // no trailing slash
+	'path' => '/Users/xestevev/Music', // no trailing slash
 );
 
 
