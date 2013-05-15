77c810d5229cbbae253f90824ced0e01d3cc1dd6
Mark Suliga <mark.suliga@fitbug.com>
2013-05-15 15:00:59 +0100

    fixed php warning when working with not existing directory

--- opensound.php
+++ opensound.php
@@ -14,8 +14,8 @@ class OpenSound {
 
 	public function files($dir = ".", $ext = 'mp3') {
 		// Returns array of files (JSON should be done in index.php)
-		$files = array();
-		if ($handle = opendir($dir)) {
+		$files = array();		
+		if ( is_dir($dir) && $handle = opendir($dir)) {
 			while (false !== ($file = readdir($handle))) {
 				if ($file != "." && $file != ".." && strtolower(substr($file, strrpos($file, '.') + 1)) == $ext) {
 					array_push($files, $file);
